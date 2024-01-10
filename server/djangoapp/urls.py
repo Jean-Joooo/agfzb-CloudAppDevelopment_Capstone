a51cca744d4f8d9ecd1b9097acbc1db33412bf3e
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-10 21:30:21 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -32,7 +32,6 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
     path('admin/', admin.site.urls),
     path('templates/', include('templates.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
