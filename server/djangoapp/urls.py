4e8e683ee680f6d1db2f8508bd9386557746fff9
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-11 04:58:08 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,21 +8,22 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
-    path('index/', views.index, name='index'),
+  
+   path('index/', views.index, name='index'),
 
     path('about/', views.about, name='about'),
 
     path('contact/', views.contact, name='contact'),
 
     path('registration/', views.registration_request, name='registration'),
-    path('login/', views.login_request, name='login'),
+     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
 
     # path for login
 
     # path for logout
 
+    path('index/', views.index, name='Index'),
    # path for add a review view
     path('add_review/', views.add_review, name='add a review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
