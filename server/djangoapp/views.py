88f92961023bdec58d6c5ef2300d2ae6beb98d08
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-11 01:56:08 +0100

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,6 +14,52 @@ import json
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect("onlinecourse:index")
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/registration.html', context)
+
+
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('onlinecourse:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/user_login.html', context)
+    else:
+        return render(request, 'djangoapp/user_login.html', context)
+
+
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
+
 
 # Create your views here.
 def index(request):
@@ -29,9 +75,6 @@ def about(request):
     context = {}
     return render(request, 'djangoapp/about.html', context) 
 
-def registration(request):
-    context = {}
-    return render(request, 'djangoapp/registration.html', context) 
 
 # Create a `contact` view to return a static contact page
 #def contact(request):
