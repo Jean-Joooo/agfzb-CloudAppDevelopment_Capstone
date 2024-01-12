ddadec08609d3251e4328d29f547d6d0ad81e60b
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-12 03:13:28 +0100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -4,17 +4,16 @@ import json
 from requests.auth import HTTPBasicAuth
 
 
-# Create a `get_request` to make HTTP GET requests
-# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-#                                     auth=HTTPBasicAuth('apikey', api_key))
+response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+                                   auth=HTTPBasicAuth('apikey', api_key))
 
-
-# Create a `post_request` to make HTTP POST requests
-# e.g., response = requests.post(url, params=kwargs, json=payload)
+response = requests.post(url, params=kwargs, json=payload)
 
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
-# def get_dealers_from_cf(url, **kwargs):
+def get_dealers_from_cf(url, **kwargs):
+    dealer_doc = dealer
+    results = []
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
 
@@ -29,6 +28,3 @@ from requests.auth import HTTPBasicAuth
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
-
-
