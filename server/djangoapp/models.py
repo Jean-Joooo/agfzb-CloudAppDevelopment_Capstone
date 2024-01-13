4261b22ec6eb5fb5966a3447edd9ff1a98248552
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-13 12:16:16 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,26 +5,23 @@ from django.utils.timezone import now
 # Create your models here.
 
 class CarMake(models.Model):
-    name = models.CharField(null=False, max_length=30)
-    description = models.CharField(max_length=1000)
-
+    name=models.CharField(null=False, max_length=30, default='CarMake')
+    description=models.CharField(max_length=1000)
     def __str__(self):
         return "Name: " + self.name + "," + \
                "Description: " + self.description
-    
 
-class CarModel(models.Model): 
-    name = models.CharField(null=False, max_length=30)
-    dealer_id = models.IntegerField(max_length=30)
-    car_type = models.CharField(max_length=30)
-    year = models.DateField(null=True)
-
+class CarModel(models.Model):
+    carmake=models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name=models.CharField(null=False, max_length=30, default='CarModel')
+    dealerid=models.IntegerField(default=0)
+    cartype=models.CharField(max_length=30)
+    year=models.DateField(default=now)
     def __str__(self):
         return "Name: " + self.name + "," + \
-               "Dealer ID: " + str(self.dealer_id) + "," + \
-               "Car Type: " + self.car_type + "," + \
-               "Year: " + str(self.year) + "," +   
-    
+               "CarType: " + self.cartype + "," + \
+               "Year: " + str(self.year.year)
+
 class CarDealer:
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
         # Dealer address
