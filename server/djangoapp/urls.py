fd1068b182897faa6712a789fe82e112f69a5c36
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-13 05:20:52 +0100

    Add files via upload

---
+++ server/djangoapp/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+app_name = 'djangoapp'
+urlpatterns = [
+    # route is a string contains a URL pattern
+    # view refers to the view function
+    # name the URL
+  
+   path('index/', views.index, name='index'),
+
+    path('about/', views.about, name='about'),
+
+    path('contact/', views.contact, name='contact'),
+
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+
+    # path for login
+
+    # path for logout
+
+    path('index/', views.index, name='Index'),
+   # path for add a review view
+    path('add_review/', views.add_review, name='add a review'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
