56b64d5b321ee7e061aa2b2f9545a8016d27d8d2
jean-joooo <jeanjosephagostini@outlook.com>
2024-01-15 06:22:56 -0500

    Adding temporary changes to Github

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -89,10 +89,12 @@ def get_dealer_by_id_from_cf(url, id):
 
     if json_result:
         dealers = json_result
-        dealer_doc = dealers
+        
+    
+        dealer_doc = dealers[0]
         dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
                                 id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"], full_name=dealer_doc["full_name"],
-                                st=dealer_doc["st"], zip=dealer_doc["zip"], short_name=dealer_doc.get("short_name"))
+                                st=dealer_doc["st"], zip=dealer_doc["zip"])
     return dealer_obj
 
 
