972a2e43db819f5bd6177873b4689641d2312258
jean-joooo <jeanjosephagostini@outlook.com>
2024-01-15 02:59:59 -0500

    Adding temporary changes to Github

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,7 +3,7 @@ import json
 from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson import NaturalLanguageUnderstandingV2
 from ibm_watson.natural_language_understanding_v2 import Features,SentimentOptions
 import time
 
