f87884323d951bb65b542825f3d78564abe0ddc4
jean-joooo <jeanjosephagostini@outlook.com>
2024-01-15 01:16:30 -0500

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.core import serializers
+from django.utils.timezone import now
+import uuid
+import json
+
+from django.db import models
 from django.utils.timezone import now
 import datetime
 
@@ -37,31 +43,70 @@ class CarModel(models.Model):
     def __str__(self):
         return self.name + ", " + str(self.year) + ", " + self.model_ty
 
+
 class CarDealer:
-    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+
+    def __init__(self, address, city, id, lat, long, st, zip, full_name):
         # Dealer address
         self.address = address
         # Dealer city
         self.city = city
-        # Dealer Full Name
-        self.full_name = full_name
+       
         # Dealer id
         self.id = id
         # Location lat
         self.lat = lat
         # Location long
         self.long = long
-        # Dealer short name
-        self.short_name = short_name
+
         # Dealer state
         self.st = st
         # Dealer zip
         self.zip = zip
+
+        # Full name
+        self.full_name = full_name
+
     def __str__(self):
         return "Dealer name: " + self.full_name
 
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class DealerReview:
+
+    def __init__(self, dealership, name, purchase, review):
+        # Required attributes
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        # Optional attributes
+        self.purchase_date = ""
+        self.purchase_make = ""
+        self.purchase_model = ""
+        self.purchase_year = ""
+        self.sentiment = ""
+        self.id = ""
+
+    def __str__(self):
+        return "Review: " + self.review
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                            sort_keys=True, indent=4)
 
 
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+class ReviewPost:
+
+    def __init__(self, dealership, name, purchase, review):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = ""
+        self.car_make = ""
+        self.car_model = ""
+        self.car_year = ""
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                            sort_keys=True, indent=4)
