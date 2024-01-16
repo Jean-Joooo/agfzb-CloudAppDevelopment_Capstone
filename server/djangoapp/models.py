b226c05bbb40846fd7fea00020ab669ae36364c6
jean-joooo <jeanjosephagostini@outlook.com>
2024-01-16 16:03:07 -0500

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,7 +4,6 @@ from django.utils.timezone import now
 import uuid
 import json
 
-
 class CarMake(models.Model):
     name = models.CharField(null=False, max_length=30, default='undifined')
     description = models.CharField(max_length=1000)
@@ -26,6 +25,7 @@ class CarModel(models.Model):
         name = models.CharField(max_length=30, default='undifined')
         type = models.CharField(null=False, max_length=20, choices= CAR_TYPE_CHOICES, default='SEDAN')
         year = models.DateField(default=now, editable=True)
+        
         def __str__(self):
             return self.name
 
@@ -54,53 +54,72 @@ class CarDealer:
     def __str__(self):
         return "Dealer name: " + self.full_name
     
+    def to_json(self):
+            return json.dumps(self, default=lambda o: o.__dict__,
+                                sort_keys=True, indent=4)
+    
 class DealerReview:
 
-        def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment):
-            # Dealership id
-            self.dealership = dealership
-            # Reviewer name
-            self.name = name
-            # Reviewer purchase date
-            self.purchase = purchase
-            # Review text
-            self.review = review
-            # Reviewer purchase date
-            self.purchase_date = purchase_date
-            # Car Make
-            self.car_make = car_make
-            # Car Model
-            self.car_model = car_model
-            # Car Year
-            self.car_year = car_year
-            # Sentiment
-            self.sentiment = sentiment
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        # Dealership id
+        self.dealership = dealership
+        # Review id
+        self.id = id
+        # Reviewer name
+        self.name = name
+        # Purchase
+        self.purchase = purchase
+        # Review
+        self.review = review
+        # Purchase date
+        self.purchase_date = purchase_date
+        # Car make
+        self.car_make = car_make
+        # Car model
+        self.car_model = car_model
+        # Car year
+        self.car_year = car_year
+        # Sentiment
+        self.sentiment = sentiment
+        # id
+        self.id = id
 
-        def __str__(self):
-            return "Review: " + self.review
+    def __str__(self):
+        return "Review: " + self.review
     
-        
+    def to_json(self):
+            return json.dumps(self, default=lambda o: o.__dict__,
+                                sort_keys=True, indent=4)
+    
 class ReviewPost:
 
-        def __init__(self, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year):
-            # Reviewer name
-            self.name = name
-            # Dealership id
-            self.dealership = dealership
-            # Review text
-            self.review = review
-            # Reviewer purchase date
-            self.purchase = purchase
-            # Reviewer purchase date
-            self.purchase_date = purchase_date
-            # Car Make
-            self.car_make = car_make
-            # Car Model
-            self.car_model = car_model
-            # Car Year
-            self.car_year = car_year
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        # Dealership id
+        self.dealership = dealership
+        # Review id
+        self.id = id
+        # Reviewer name
+        self.name = name
+        # Purchase
+        self.purchase = purchase
+        # Review
+        self.review = review
+        # Purchase date
+        self.purchase_date = purchase_date
+        # Car make
+        self.car_make = car_make
+        # Car model
+        self.car_model = car_model
+        # Car year
+        self.car_year = car_year
+        # Sentiment
+        self.sentiment = sentiment
+        # id
+        self.id = id
 
-        
-        def to_json(self):
+    def __str__(self):
+        return "Review: " + self.review
+    
+    def to_json(self):
             return json.dumps(self, default=lambda o: o.__dict__,
-                                sort_keys=True, indent=4)   
+                                sort_keys=True, indent=4)
